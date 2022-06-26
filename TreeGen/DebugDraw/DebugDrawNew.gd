63d4956191436bb65e0fe7fcdab843b7b200b25c
Oblivious-Vagabond <photxx@gmail.com>
2022-06-26 21:34:53 +0200

    add line strip draw functionality

--- TreeGen/DebugDraw/DebugDrawNew.gd
+++ TreeGen/DebugDraw/DebugDrawNew.gd
@@ -7,6 +7,11 @@ func new_point_cloud(pos, p_size, color):
 	add_child(point_cloud)
 	return point_cloud
 
+func new_line_seg(pos, color):
+	var line_seg = LineSegment.new(color)
+	line_seg.position = pos
+	add_child(line_seg)
+	return line_seg
 
 class PointCloud extends MeshInstance3D:
 	var cloud:PackedVector3Array
@@ -39,3 +44,26 @@ class PointCloud extends MeshInstance3D:
 		mesh_imm.surface_set_material(0,mat)
 		print(mesh_imm.surface_get_material(0))
 		set_mesh(mesh_imm)
+
+class LineSegment extends MeshInstance3D:
+	var points:PackedVector3Array
+	var mat = StandardMaterial3D.new()
+	
+	func _init(color):
+		mat.albedo_color = color
+	
+	func add_point(point:Vector3): points.push_back(point)
+	func add_points(new_points:PackedVector3Array): points += new_points
+	func set_points(new_points:PackedVector3Array): points = new_points
+	
+	func construct():
+		if points.size() == 1: points.append(points[0])
+		var mesh_imm = ImmediateMesh.new()
+		mesh_imm.surface_begin(Mesh.PRIMITIVE_LINE_STRIP)
+		mesh_imm.surface_set_normal(Vector3(0,0,1))
+		for p in points:
+			mesh_imm.surface_add_vertex(p)
+		mesh_imm.surface_end()
+		mesh_imm.surface_set_material(0,mat)
+		set_mesh(mesh_imm)
+		
